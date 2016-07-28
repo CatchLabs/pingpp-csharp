12a89ad8f686a75435868d2441344791f47026b8
Richard <richard9372@gmail.com>
2016-07-28 14:55:38 +0800

    Add project files.

---
+++ src/PingppNetcoreSdk/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PingppNetcoreSdk
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
